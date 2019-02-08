1359b662e5bda6db749f8a8d59eec87ad72877a7
nguyenngocha <dodaihoc.abvk@gmail.com>
2019-02-08 14:45:26 +0000

    fix8

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,8 +8,8 @@ every 5.minutes do
   rake "job:auto_upgrate_random_dorf1"
 end
 
-# every 10.minutes do
-#   rake "job:auto_upgrate_random_dorf1"
-# end
+every 13.minutes do
+  rake "rake job:farm_oasis"
+end
 
 # whenever --update-crontab --set environment=development
