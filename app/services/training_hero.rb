9bb74895731a8dac62270be398409ecd5d8158d7
nguyenngocha <dodaihoc.abvk@gmail.com>
2019-03-03 06:32:54 +0000

    fix

--- app/services/training_hero.rb
+++ app/services/training_hero.rb
@@ -37,7 +37,7 @@ class TrainingHero
     end
     puts target_def
     puts @hero_dame
-    if  100 < target_def and target_def < @hero_dame/6
+    if  200 < target_def and target_def < @hero_dame/5
       puts "true"
       return true
     end
