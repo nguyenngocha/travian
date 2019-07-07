c27425dd9429806ed52a7e47f5b3119f307b0fe7
vagrant <vagrant@ubuntu>
2019-07-07 14:58:10 +0000

    fix

--- app/services/farm_gold.rb
+++ app/services/farm_gold.rb
@@ -75,7 +75,7 @@ class FarmGold
       "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
     }
     puts "RestClient.post(#{@user.server}/build.php?gid=16&tt=99, #{params}, cookies: #{@cookies})"
-    # RestClient.post("#{@user.server}/build.php?gid=16&tt=99", params, @headers)
+    RestClient.post("#{@user.server}/build.php?gid=16&tt=99", params, @headers)
     sleep 1
   end
 end
